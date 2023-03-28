85630640edfb0e52a8a139543b2da424b262c677
Admin Server <apostlma@ucsd.edu>
2023-03-28 11:53:32 +0000

    added Car component

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Car from "./components/Car"
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title} >Welcome Class of Stellantis-OU Module 3!</Text>
+      <Car></Car>
     </View>
   );
+
+
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +20,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 25,
+    color: "#b3a33e"
+  },
+  title2: {
+    fontSize: 36, 
+    color: "#21397d"
   },
 });
---
+++ components/Car.js
@@ -0,0 +1,25 @@
+// import dependencies
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React from "react";
+
+// define & build (& export) functionality of component
+export default class Car extends React.Component{
+
+    // work with properties & states for GUI
+
+
+    // render our GUI
+    render(){
+        return(
+            <View>
+                <Button title="On"></Button>
+            </View>
+        )
+    }
+    
+
+
+}
+
+
+// use Stylesheet to style our component
